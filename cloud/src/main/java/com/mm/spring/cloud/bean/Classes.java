db16e60e78fdb35a1fd10cc74c149dcb4cede064
wang.meng <wangmeng@email.com>
2018-11-16 16:17:06 +0800

    配置mybatis generator

--- cloud/src/main/java/com/mm/spring/cloud/bean/Classes.java
+++ cloud/src/main/java/com/mm/spring/cloud/bean/Classes.java
@@ -1,32 +1,38 @@
 package com.mm.spring.cloud.bean;
 
+import javax.persistence.*;
+
 public class Classes {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String classes;
 
-    public Classes(Long id, String classes) {
-        this.id = id;
-        this.classes = classes;
-    }
-
-    public Classes() {
-        super();
-    }
-
+    /**
+     * @return id
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * @param id
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * @return classes
+     */
     public String getClasses() {
         return classes;
     }
 
+    /**
+     * @param classes
+     */
     public void setClasses(String classes) {
-        this.classes = classes == null ? null : classes.trim();
+        this.classes = classes;
     }
 }
